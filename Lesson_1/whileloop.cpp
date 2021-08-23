044ff04370e22a66f6333b136fbf03b4c98a5e3d
Elder Pereira <elder-guedes@hotmail.com>
2021-08-22 22:03:51 -0300

    While loop

---
+++ Lesson_1/whileloop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //the program will interate one up to 500 in our counter variable and show what number is divisible by five and three
+   int counter = 100;
+   while (counter< 500)
+   {
+       if (counter< 500)
+        {
+            counter= counter +1;
+            if (counter%3 == 0 and counter%5 == 0)
+            {
+                cout <<  counter << " ";
+            }
+        }
+        
+   }
+   
+   
+}
